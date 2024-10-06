28c8674a91c43f91a6ebba61a4ed79efa50dea85
Dikshant Singh <95041015+elit3pwner@users.noreply.github.com>
2024-10-06 11:55:26 +0000

    sort endpoints

---
+++ sort.sh
@@ -0,0 +1,28 @@
+# Basic script to filter out potential endpoints
+
+# XSS
+echo "Filtering URLs for potential XSS endpoints..."
+cat "output.txt" | Gxss | kxss | grep -oP '^URL: \K\S+' | sed 's/=.*/=/' | sort -u > "$output_dir/xss_output.txt"
+echo "Extracting final filtered URLs to xss_output.txt..."
+
+# Open Redirect
+echo "Filtering URLs for potential Open Redirect endpoints..."
+cat "output.txt" | gf or | sed 's/=.*/=/' | sort -u > "open_redirect_output.txt"
+
+# LFI
+echo "Filtering URLs for potential LFI endpoints..."
+cat "output.txt" | gf lfi | sed 's/=.*/=/' | sort -u > "lfi_output.txt"
+
+# SQLi
+echo "Filtering URLs for potential SQLi endpoints..."
+cat "output.txt" | gf sqli | sed 's/=.*/=/' | sort -u > "sqli_output.txt"
+
+# Remove the intermediate file output.txt
+rm "output.txt"
+
+# Notify the user that all tasks are complete
+echo "Filtered URLs have been saved to the respective output files:"
+echo "  - XSS: xss_output.txt"
+echo "  - Open Redirect: open_redirect_output.txt"
+echo "  - LFI: lfi_output.txt"
+echo "  - SQLi: sqli_output.txt"
